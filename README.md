a6a775647e60166bd8435caebded5258dba8e738
Sergejs Kostjucenko <sergejs@parity.io>
2021-12-02 13:20:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# PR Special Review
+
+This is an action created for for complex pull request approval cases that are not currently supported by the [protected branches](https://docs.github.com/en/github/administering-a-repository/defining-the-mergeability-of-pull-requests/about-protected-branches#about-branch-protection-settings) feature in GitHub.
+
+## How this action works
+
+This action is intended to be executed every time some change is made to the pull request (see [workflow example](#Workflow-config])). When this action is executed, it checks whether the review and approval status of the triggered pull request meets the policy described in the [action's config](#Action-config), and sets the result to a commit status named "PR Gatekeeper Status".
+
+
+You can enforce the review policy described in action config by setting this "PR Gatekeeper Status" as required in the protected branch settings.
+
+## Configuration
+
+### Action config
+
+The action is configured via the `approve_config.yml` file located in the `.github` subdirectory. Default config file can be overriden in workflow inputs.
+The general format is as follows.
+
+```yaml
+approvals:
+  # check will fail if there is no approval
+  minimum: 1     # optional - the same as repo protected branch settings
+  groups:
+    - name: reviewers_group1
+      minimum: 1 # number of needed approvals
+      from:
+        person: # list of individual users to request and check approvals
+          - user1
+          - user2
+        team: # list of teams to request and check approvals works only when token with special scope used
+          - team1
+          - team2
+    - name: reviewers_group2
+      minimum: 2
+      from:
+        - user3
+        - user4
+```
+
+### Workflow config
+
+Once the `approve_config.yml` file is in place, add the action to execute on every PR and then set it as a required action to start enforcing your new approval policy!
+
+```yaml
+name: 'PR Gatekeeper'
+
+on:
+  pull_request:
+    types:
+      [
+        assigned,
+        unassigned,
+        opened,
+        reopened,
+        synchronize,
+        review_requested,
+        review_request_removed
+      ]
+  pull_request_review:
+
+jobs:
+  pr-gatekeeper:
+    name: PR Gatekeeper
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: sergejparity/pr-special-review@master
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          # OPTIONAL config-file: './.github/approve_config.yml'
+```
---
+++ src/main.ts
@@ -0,0 +1,153 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as Webhooks from '@octokit/webhooks-types'
+import * as fs from 'fs'
+import * as YAML from 'yaml'
+import { EOL } from 'os'
+import { Settings, ReviewGatekeeper } from './review_gatekeeper'
+
+export async function assignReviewers(client: any,
+                                      reviewer_persons: string[],
+                                      reviewer_teams: string[],
+                                      pr_number: number) {
+  try {
+    console.log(`entering assignReviewers`)
+    if (reviewer_persons.length || reviewer_teams.length) {
+      await client.rest.pulls.requestReviewers({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr_number,
+        reviewers: reviewer_persons[0],
+        // UNCOMMENT IF USING PAT TOKEN team_reviewers: reviewer_teams[0],
+      });
+      core.info(`Requested review from: ${reviewer_persons}.`);
+      // UNCOMMENT IF USING PAT TOKEN core.info(`Assigned team reviews to ${reviewer_teams}.`)
+    }
+    console.log(`exiting assignReviewers`)
+  } catch (error) {
+    core.setFailed(error.message)
+    console.log("error: ", error)
+  }
+}
+
+async function run(): Promise<void> {
+  try {
+    const context = github.context
+    if (
+      context.eventName !== 'pull_request' &&
+      context.eventName !== 'pull_request_review'
+    ) {
+      core.setFailed(
+        `Invalid event: ${context.eventName}. This action should be triggered on pull_request and pull_request_review`
+      )
+      return
+    }
+    const payload = context.payload as
+      | Webhooks.PullRequestEvent
+      | Webhooks.PullRequestReviewEvent
+
+    const token: string = core.getInput('token')
+    const octokit = github.getOctokit(token)
+    const pr_number = payload.pull_request.number
+    const sha = payload.pull_request.head.sha
+    const workflow_url = `${process.env['GITHUB_SERVER_URL']}/${process.env['GITHUB_REPOSITORY']}/actions/runs/${process.env['GITHUB_RUN_ID']}`
+
+    // Read values from config file if it exists
+    const config_file = fs.readFileSync(core.getInput('config-file'), 'utf8')
+
+    // Parse contents of config file into variable
+    const config_file_contents = YAML.parse(config_file)
+
+    const reviewer_persons: string[] = []
+    for (const persons of config_file_contents.approvals.groups) {
+      reviewer_persons.push(persons.from.person)
+    }
+
+    const reviewer_teams: string[] = []
+    for (const teams of config_file_contents.approvals.groups) {
+      reviewer_teams.push(teams.from.team)
+    }
+
+    // const reviewsParam = {
+    //   ...context.repo,
+    //   pull_number: pr_number,
+    // };
+    // const reviewsResponse = await octokit.rest.pulls.listReviews(reviewsParam);
+
+    // const reviews = new Map();
+    // reviewsResponse.data.forEach(review => {
+    //   reviews.set(review.user?.login, review.state);
+    // });
+
+    // core.info(`Latest Reviews`);
+    // reviews.forEach((value, key) => {
+    //   core.info(`${key} = ${value}`);
+    // });
+
+    // Request reviews if eventName == pull_request
+    if (context.eventName == 'pull_request') {
+      console.log(`We are going to request someones approval!!!`)
+      assignReviewers(octokit, reviewer_persons, reviewer_teams, pr_number)
+
+      octokit.rest.repos.createCommitStatus({
+        ...context.repo,
+        sha,
+        state: 'failure',
+        context: 'PR Gatekeeper Status',
+        target_url: workflow_url,
+        description: "PR contains changes subject to special review"
+      })
+
+    }
+
+    console.log(`We don't care about requesting approvals! We'll just check who already approved`)
+
+    //retrieve approvals
+    const reviews = await octokit.rest.pulls.listReviews({
+      ...context.repo,
+      pull_number: payload.pull_request.number
+    })
+    const approved_users: Set<string> = new Set()
+    for (const review of reviews.data) {
+      if (review.state === `APPROVED`) {
+        approved_users.add(review.user!.login)
+        console.log(`Approval from: ${review.user!.login}`)
+      }
+    }
+
+    // check approvals
+    const review_gatekeeper = new ReviewGatekeeper(
+      config_file_contents as Settings,
+      Array.from(approved_users),
+      payload.pull_request.user.login
+    )
+
+    console.log(`sha: ${sha}`)
+    // The workflow url can be obtained by combining several environment varialbes, as described below:
+    // https://docs.github.com/en/actions/reference/environment-variables#default-environment-variables
+    console.log(`workflow_url: ${workflow_url}`)
+    core.info(`Setting a status on commit (${sha})`)
+
+
+    octokit.rest.repos.createCommitStatus({
+      ...context.repo,
+      sha,
+      state: review_gatekeeper.satisfy() ? 'success' : 'failure',
+      context: 'PR Gatekeeper Status',
+      target_url: workflow_url,
+      description: review_gatekeeper.satisfy()
+        ? undefined
+        : review_gatekeeper.getMessages().join(' ').substr(0, 140)
+    })
+
+    if (!review_gatekeeper.satisfy() && context.eventName == 'pull_request_review') {
+      core.setFailed(review_gatekeeper.getMessages().join(EOL))
+      return
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+    console.log("error: ", error)
+  }
+}
+
+run()
